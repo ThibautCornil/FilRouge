4803e33599a300ab5113b6a76f4dd3dcf29c7b59
Thibaut Cornil <thibaut.cornil@hotmail.fr>
2022-01-31 18:00:06 +0100

    mise a jour menu

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-
 const btn = document.querySelector('.btn1');
+const menu = document.querySelector(".menu")
 
 btn.addEventListener('click', presentation)
 
 function presentation() {
     btn.classList.toggle('active'); //on / off avec toggle
-
+    if(getComputedStyle(menu).display != "none"){
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
 }
 
