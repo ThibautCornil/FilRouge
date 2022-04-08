4b545496169f4ecb6e727d0a9fea872f37ae4807
Thibaut Cornil <thibaut.cornil@hotmail.fr>
2022-04-08 11:29:05 +0200

    ajout overflow

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector('.btn1');
-const menu = document.querySelector(".menu")
+const menu = document.querySelector(".menu");
+const html = document.querySelector("html");
 
 btn.addEventListener('click', presentation)
 
@@ -7,8 +8,11 @@ function presentation() {
     btn.classList.toggle('active'); //on / off avec toggle
     if(getComputedStyle(menu).display != "none"){
         menu.style.display = "none";
+        html.style.overflow = "visible";
     } else {
         menu.style.display = "block";
+        html.style.overflow = "hidden";
+
     }
 }
 
